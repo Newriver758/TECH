82ef32162103a683d2991a8ed828a21654cf6fc8
Newriver758 <shinkawa0109@gmail.com>
2024-10-06 11:04:51 +0900

    コントローラー、モデルのバリデーション

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,17 @@
 class AnswersController < ApplicationController
   
   def create
-    Answer.create(answer_params)
-    redirect_to question_path(params[:question_id]) 
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.build(answer_params)
+    @answer.user = current_user # ユーザー関連付けが必要な場合
+
+    if @answer.save
+      redirect_to question_path(@question), notice: '回答が投稿されました。'
+    else
+      render 'questions/show', alert: '回答の投稿に失敗しました。'
+    end
   end
+
 
   private
   def answer_params
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   validates :body, presence: true
+  validates :content, presence: true
 end
